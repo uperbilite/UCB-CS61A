54041c65c857ba93eae0d7ce65c04e7ea6a45972
uperbilite <uperbilite@gmail.com>
2022-09-13 15:52:12 +0800

    lab14

---
+++ lab/lab14/lab14.scm
@@ -0,0 +1,26 @@
+(define (split-at lst n) 
+  (define (first lst n)
+	(if (= n 0)
+		nil
+		(cons (car lst) (first (cdr lst) (- n 1)))))
+  (define (remain lst n)
+	(if (= n 0)
+		(if (= 0 (length lst))
+			nil
+			(cons (car lst) (remain (cdr lst) n)))
+		(remain (cdr lst) (- n 1))))
+  (if (> n (length lst))
+	  (cons lst nil)
+	  (cons (first lst n) (remain lst n))))
+
+(define (compose-all funcs) 
+  (define x (lambda(x) x))
+  (define (compose funcs x)
+	(if (eq? funcs nil)
+		x
+		(if (eq? (cdr funcs) nil)
+			((car funcs) x)
+			(compose (cdr funcs) ((car funcs) x)))))
+  (define (procedure x)
+	(compose funcs x))
+  procedure)
---
+++ lab/lab14/lab14.sql
@@ -0,0 +1,23 @@
+create table pizzas as
+  select "Pizzahhh" as name, 12 as open, 15 as close union
+  select "La Val's"        , 11        , 22          union
+  select "Sliver"          , 11        , 20          union
+  select "Cheeseboard"     , 16        , 23          union
+  select "Emilia's"        , 13        , 18;
+
+create table meals as
+  select "breakfast" as meal, 11 as time union
+  select "lunch"            , 13         union
+  select "dinner"           , 19         union
+  select "snack"            , 22;
+
+
+-- Pizza places that open before 1pm in alphabetical order
+create table opening as
+SELECT name FROM pizzas WHERE open < 13 ORDER BY name DESC;
+
+
+-- Two meals at the same place
+create table double as
+SELECT m1.meal AS earlier, m2.meal AS later, name AS place FROM meals AS m1, meals AS m2, pizzas WHERE m2.time - m1.time > 6 AND m2.time >= open AND m2.time <= close AND m1.time >= open AND m2.time <= close;
+
